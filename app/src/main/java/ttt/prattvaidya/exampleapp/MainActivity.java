c547b9cb4d8eb6623bbe2c6f184096b4b9f5dd68
Pratt Vaidya <pratt.vaidya@ttt.studio>
2021-07-23 09:24:55 -0700

    Get value from a textfield and log it

--- app/src/main/java/ttt/prattvaidya/exampleapp/MainActivity.java
+++ app/src/main/java/ttt/prattvaidya/exampleapp/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,5 +16,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void handleClick(View view) {
         Log.i("Info", "Button pressed");
+        EditText nameEditText = (EditText) findViewById(R.id.nameEditText);
+        Log.i("Values",nameEditText.getText().toString());
     }
 }
